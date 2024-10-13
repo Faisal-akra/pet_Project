06144c8559938df032e6ba452efae0fcb11f1110
Faisal-Akram <zjan45097@gmail.com>
2024-10-13 10:25:56 +0500

    play method

--- java.js
+++ java.js
@@ -3,6 +3,19 @@ var pet = {
     age: 3,
     happiness: 50,
     hunger:80,
-    
+
+
+    feed: function() {
+        this.hunger = Math.max(this.hunger -20, 0)
+    },
+
+    play: function() {
+        this.happiness = Math.min(this.happiness +20, 100)
+    },
+
+    agePet: function() {
+        
+    }
 
 }
+    
