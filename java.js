18fd69ace66e310d21af005469231d722e82d0b2
Faisal-Akram <zjan45097@gmail.com>
2024-10-13 10:34:45 +0500

    methods is completed

--- java.js
+++ java.js
@@ -6,15 +6,22 @@ var pet = {
 
 
     feed: function() {
-        this.hunger = Math.max(this.hunger -20, 0)
+        this.hunger = Math.max(this.hunger -20, 0);
+        alert(`${this.name}  has hunger now ${this.hunger}`)
     },
 
     play: function() {
-        this.happiness = Math.min(this.happiness +20, 100)
+        this.happiness = Math.min(this.happiness +20, 100);
+        alert(`${this.name} has happy now ${this.happiness}`)
     },
 
     agePet: function() {
-        
+        this.age +=1;
+        this.happiness = Math.max(this.happiness -5, 0);
+        this.hunger = Math.min(this.hunger +10, 100);
+
+        alert(`${this.name} is now ${this.age} old and happiness is ${this.happiness}`)
+
     }
 
 }
